80a6408f544d8dce2f3f5503acf83be0f31949aa
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-11 11:48:11 -0400

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,28 @@ require 'pry' # using 'binding.pry to debug'
 
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App, #{@user.first_name}!"
+      render :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    # use strong params below:
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
